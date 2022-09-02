e3e3c798bd7383a5169edd49d3d13e7ea08c851f
barreto <gbarreto@keener.io>
2022-09-02 18:29:22 -0300

    change: componentizaçao skill cards

---
+++ src/components/Landing/components/SkillCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="text-center skill-card">
+      <icon>
+        <slot name="icon"></slot>
+      </icon>
+      <title-skill>
+        <p class="text-center skill-card-title">
+          <slot name="title-skill"></slot>
+        </p>
+      </title-skill>
+      <card-text>
+        <p class="skill-card-text"><slot name="card-text"></slot></p>
+      </card-text>
+      <expertise-title>
+        <p class="text-center skill-card-subtitle">
+          <slot name="expertise-title"></slot>
+        </p>
+      </expertise-title>
+      <expertise>
+        <p class="skill-card-text">
+          <slot name="expertise"></slot>
+        </p>
+      </expertise>
+      <tools-name>
+        <p class="text-center skill-card-subtitle">
+          <slot name="tools-name"></slot>
+        </p>
+      </tools-name>
+      <tools>
+        <slot name="tools"></slot>
+      </tools>
+    </div>
+  </div>
+</template>
+<script>
+'SkillCard';
+</script>
--- src/components/Landing/sections/SkillsSection.vue
+++ src/components/Landing/sections/SkillsSection.vue
@@ -1,115 +1,108 @@
 <template>
   <div class="container vh-100">
     <div class="row h-100 align-items-center">
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="text-center skill-card">
-          <div>
-            <font-awesome-icon
-              class="skill-card-icon"
-              icon="fa-solid fa-gears"
-            />
-            <p class="text-center skill-card-title">Engenharia</p>
-            <p class="skill-card-text">
-              Do IoT ao projeto de peças sob medida para as mais
-              diversas aplicações. Sempre prezo pela qualidade e
-              eficiência dos trabalhos que desenvolvo, buscando o
-              equilíbrio entre orçamento e qualidade.
-            </p>
-            <p class="text-center skill-card-subtitle">
-              Especialidade
-            </p>
-            <p class="skill-card-text">
-              Soluções em automação residencial e do campo, projetos
-              sob medida, soluções em eletrônica automotiva.
-            </p>
-            <p class="text-center skill-card-subtitle">
-              Principais Hardwares e Ferramentas
-            </p>
-            <ul>
-              <li class="text-start skill-card-text">
-                STM32*, ATMEGA*, ESP*, PIC16F*
-              </li>
-              <li class="text-start skill-card-text">
-                CAN, MQQT, Tasmota
-              </li>
-              <li class="text-start skill-card-text">
-                EasyEDA, Eagle, Fusion360
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="text-center skill-card">
-          <div class="">
-            <font-awesome-icon
-              class="skill-card-icon"
-              icon="fa-solid fa-code"
-            />
-            <p class="text-center skill-card-title">
-              Desenvolvimento
-            </p>
-            <p class="skill-card-text">
-              Frontend, Backend ou Mobile. Seja a vertente desenvolvo
-              com TDD o seu projeto de ponta a ponta, com a qualidade
-              necessária para futuras manutenções e implementação de
-              novas features.
-            </p>
-            <p class="text-center skill-card-subtitle">
-              Especialidade
-            </p>
-            <p class="skill-card-text">
-              Do mais simples gerenciador de músicas de karaokê ao
-              mais completo sistema de backoffice.
-            </p>
-            <p class="text-center skill-card-subtitle">
-              Principais Tecnologias
-            </p>
-            <ul>
-              <li class="text-start skill-card-text">
-                Vanilla, Vue, Nuxt
-              </li>
-              <li class="text-start skill-card-text">
-                Flutter, Android Native
-              </li>
-              <li class="text-start skill-card-text">
-                Node + Express, C# + Entity Framework
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="text-center skill-card">
-          <div>
-            <font-awesome-icon
-              class="skill-card-icon"
-              icon="fa-solid fa-user-ninja"
-            />
-            <p class="text-center skill-card-title">Soft Skills</p>
-            <p class="skill-card-text">
-              Com comunicação e agilidade orquestro com maestria as
-              entregas do seu projeto. Sempre mantendo clareza e
-              transparência com o cliente, do contrato à entrega
-              final.
-            </p>
-            <br />
-            <p class="skill-card-text">
-              Com sede de conhecimento sempre estou em busca de novas
-              tecnologias e tendências mais atuais do mercado.
-            </p>
-            <br />
-            <p class="skill-card-text">
-              Não existe tecnologia ou paradigma que eu não encare, me
-              traga um framework obscuro e lhe entregarei um produto
-              que atenda as suas expectativas.
-            </p>
-          </div>
-        </div>
-      </div>
+      <SkillCard>
+        <template v-slot:icon>
+          <font-awesome-icon
+            class="skill-card-icon"
+            icon="fa-solid fa-gears"
+          />
+        </template>
+        <template v-slot:title-skill> Engenharia </template>
+        <template v-slot:card-text>
+          Do IoT ao projeto de peças sob medida para as mais diversas
+          aplicações. Sempre prezo pela qualidade e eficiência dos
+          trabalhos que desenvolvo, buscando o equilíbrio entre
+          orçamento e qualidade.
+        </template>
+        <template v-slot:expertise-title> Especialidade </template>
+        <template v-slot:expertise>
+          Soluções em automação residencial e do campo, projetos sob
+          medida, soluções em eletrônica automotiva.
+        </template>
+        <template v-slot:tools-name>
+          Principais Hardwares e Ferramentas
+        </template>
+        <template v-slot:tools>
+          <ul>
+            <li class="text-start skill-card-text">
+              STM32*, ATMEGA*, ESP*, PIC16F*
+            </li>
+            <li class="text-start skill-card-text">
+              CAN, MQQT, Tasmota
+            </li>
+            <li class="text-start skill-card-text">
+              EasyEDA, Eagle, Fusion360
+            </li>
+          </ul>
+        </template>
+      </SkillCard>
+
+      <SkillCard>
+        <template v-slot:icon>
+          <font-awesome-icon
+            class="skill-card-icon"
+            icon="fa-solid fa-code"
+          />
+        </template>
+        <template v-slot:title-skill> Desenvolvimento </template>
+        <template v-slot:card-text>
+          Frontend, Backend ou Mobile. Seja a vertente desenvolvo com
+          TDD o seu projeto de ponta a ponta, com a qualidade
+          necessária para futuras manutenções e implementação de novas
+          features.
+        </template>
+        <template v-slot:expertise-title> Especialidade </template>
+        <template v-slot:expertise>
+          Do mais simples gerenciador de músicas de karaokê ao mais
+          completo sistema de backoffice.
+        </template>
+        <template v-slot:tools-name>
+          Principais Tecnologias
+        </template>
+        <template v-slot:tools>
+          <ul>
+            <li class="text-start skill-card-text">
+              Vanilla, Vue, Nuxt
+            </li>
+            <li class="text-start skill-card-text">
+              Flutter, Android Native
+            </li>
+            <li class="text-start skill-card-text">
+              Node + Express, C# + Entity Framework
+            </li>
+          </ul>
+        </template>
+      </SkillCard>
+
+      <SkillCard>
+        <template v-slot:icon>
+          <font-awesome-icon
+            class="skill-card-icon"
+            icon="fa-solid fa-user-ninja"
+          />
+        </template>
+        <template v-slot:title-skill> Soft Skills </template>
+        <template v-slot:card-text>
+          Com comunicação e agilidade orquestro com maestria as
+          entregas do seu projeto. Sempre mantendo clareza e
+          transparência com o cliente, do contrato à entrega final.<br /><br />Com
+          sede de conhecimento sempre estou em busca de novas
+          tecnologias e tendências mais atuais do mercado.<br /><br />Não
+          existe tecnologia ou paradigma que eu não encare, me traga
+          um framework obscuro e lhe entregarei um produto que atenda
+          as suas expectativas.
+        </template>
+      </SkillCard>
     </div>
   </div>
 </template>
 <script>
-'SkillsSection';
+import SkillCard from '../components/SkillCard.vue';
+('SkillsSection');
+export default {
+  components: {
+    SkillCard,
+  },
+};
 </script>
